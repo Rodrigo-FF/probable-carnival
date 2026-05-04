fe2fca14a442d7a3225023441d2d37038ffc1766
Rodrigo-FF <omega.hellblazer@gmail.com>
2026-05-04 19:44:08 -0300

    Create hd_pus2.py

---
+++ hd_pus2.py
@@ -0,0 +1,4 @@
+print("Loja Rodrigo Ferreira Florencio")
+Valor_unitário = float(input("Digite o valor do produto: "  ))
+quantidade_do_produto = int(input("Digite a quantidade: "))
+valor_frete = 0
